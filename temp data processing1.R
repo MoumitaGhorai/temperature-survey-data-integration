e32b5303d17873a1c040f5fc061e56a0120d01f3
Moumita Ghorai <45401219+MoumitaGhorai@users.noreply.github.com>
2024-03-28 14:48:01 -0400

    Update temp data processing1.R

--- temp data processing1.R
+++ temp data processing1.R
@@ -4,7 +4,7 @@ library(raster) # package for raster manipulation
 library(rgdal) # package for geospatial analysis
 library(ggplot2) # package for plotting
 
-fname <- "C:/Users/moumi/Downloads/air.mon.mean (1).nc"
+fname <- "~/Downloads/air.mon.mean.nc"
 HadISST.b <- brick(fname)  
 HadISST.b
 plot(HadISST.b[[1]])
@@ -13,4 +13,4 @@ dim(HadISST.b)
 #nc_df <- as.data.frame(HadISST.b, xy=T)
 nc_df <- as.data.frame(HadISST.b, xy = TRUE, na.rm = TRUE)
 
-write.csv(nc_df, "C:/Users/moumi/Downloads/practicefilename5.csv")
+write.csv(nc_df, "~/Downloads/total_temp.csv")
